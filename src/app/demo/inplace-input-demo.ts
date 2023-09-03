0a5ec4aee0a137030c2bf4d2fb54ac5ddc64dbd4
jehanraj <jeganraj.a27@gmail.com>
2023-09-04 00:19:19 +0530

    2 files modified

--- src/app/demo/inplace-input-demo.ts
+++ src/app/demo/inplace-input-demo.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'inplace-input-demo',
-    templateUrl: './inplace-input-demo.html'
+  selector: 'inplace-input-demo',
+  templateUrl: './inplace-input-demo.html',
 })
-export class InplaceInputDemo {}
+export class InplaceInputDemo {
+  testinput = '123';
+}
